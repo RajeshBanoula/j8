39ed89a5af1365a78edd16395274b38746cb39ef
rajesh <rsbanoula@gmail.com>
2020-06-02 20:50:57 +0530

    funInterface

---
+++ src/raj/release/j8/features/functionalinterface/FunctionInterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package raj.release.j8.features.functionalinterface;
+
+/**
+ * @author rsban
+ *
+ */
+public class FunctionInterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
